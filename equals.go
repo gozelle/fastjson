ac4a89211923dbb75238b0eb368314c031b6a949
koyeo <koyeo@qq.com>
2023-01-26 14:29:04 +0800

    add equals

--- equals.go
+++ equals.go
@@ -21,20 +21,35 @@ func EqualsBytes(a, b []byte) (err error) {
 		err = fmt.Errorf("init b error: %s", err)
 		return
 	}
+	
+	path := "$"
+	
 	if va.Type() != TypeArray && va.Type() != TypeObject {
-		return equals(va, vb)
+		err = equals(path, va, vb)
+		if err != nil {
+			return
+		}
+		
 	} else if va.Type() == TypeArray {
-		return equalsArray(va, vb)
+		err = equalsArray(path, va, vb)
+		if err != nil {
+			return
+		}
+		
 	} else {
-		return equalsObject(va, vb)
+		err = equalsObject(path, va, vb)
+		if err != nil {
+			return
+		}
 	}
+	return
 }
 
-func equalsObject(va, vb *Value) (err error) {
+func equalsObject(path string, va, vb *Value) (err error) {
 	
 	ob, err := vb.Object()
 	if err != nil {
-		err = fmt.Errorf("get b object error: %s", err)
+		err = compareError(path, fmt.Errorf("get b object error: %s", err))
 		return
 	}
 	oa, _ := va.Object()
@@ -47,27 +62,29 @@ func equalsObject(va, vb *Value) (err error) {
 	}()
 	
 	if oa.Len() != ob.Len() {
-		err = fmt.Errorf("oa len: %d != ob len: %d", oa.Len(), ob.Len())
+		err = compareError(path, fmt.Errorf("object length: %d != %d", oa.Len(), ob.Len()))
 		return
 	}
 	
 	oa.Visit(func(key []byte, v *Value) {
+		p := fmt.Sprintf("%s.%s", path, string(key))
+		
 		obv := ob.Get(string(key))
 		if obv == nil {
-			panic(fmt.Errorf("b(%s) is nil", string(key)))
+			panic(compareError(p, fmt.Errorf("b.Value is nil")))
 		}
 		if v.Type() == TypeObject {
-			err = equalsObject(v, obv)
+			err = equalsObject(p, v, obv)
 			if err != nil {
 				panic(err)
 			}
 		} else if v.Type() == TypeArray {
-			err = equalsArray(v, obv)
+			err = equalsArray(p, v, obv)
 			if err != nil {
 				panic(err)
 			}
 		} else {
-			err = equals(v, obv)
+			err = equals(p, v, obv)
 			if err != nil {
 				panic(err)
 			}
@@ -77,39 +94,41 @@ func equalsObject(va, vb *Value) (err error) {
 	return
 }
 
-func equals(va, vb *Value) error {
+func equals(path string, va, vb *Value) error {
 	
 	if va.Type() == TypeArray || va.Type() == TypeObject {
-		return fmt.Errorf("internal error: sholud not use equals")
+		return compareError(path, fmt.Errorf("internal error: sholud not use equals"))
 	}
 	
 	if va.String() != vb.String() {
-		return fmt.Errorf("%s != %s", va, vb)
+		return compareError(path, fmt.Errorf("%s != %s", va, vb))
 	}
 	
 	return nil
 }
 
-func equalsArray(va, vb *Value) (err error) {
+func equalsArray(path string, va, vb *Value) (err error) {
 	
 	ab, err := vb.Array()
 	if err != nil {
-		err = fmt.Errorf("get b object error: %s", err)
+		err = compareError(path, fmt.Errorf("get b object error: %s", err))
 		return
 	}
 	aa, _ := va.Array()
 	
 	if len(aa) != len(ab) {
-		err = fmt.Errorf("array length not match, aa: %d, ab: %d", len(aa), len(ab))
+		err = compareError(path, fmt.Errorf("array length not match, aa: %d, ab: %d", len(aa), len(ab)))
 		return
 	}
+	
 	for i, v := range aa {
+		p := fmt.Sprintf("%s.%d", path, i)
 		if v.Type() == TypeObject {
-			err = equalsObject(v, ab[i])
+			err = equalsObject(p, v, ab[i])
 		} else if v.Type() == TypeArray {
-			err = equalsArray(v, ab[i])
+			err = equalsArray(p, v, ab[i])
 		} else {
-			err = equals(v, ab[i])
+			err = equals(p, v, ab[i])
 		}
 		if err != nil {
 			return
@@ -117,4 +136,8 @@ func equalsArray(va, vb *Value) (err error) {
 	}
 	
 	return
+}
+
+func compareError(path string, err error) error {
+	return fmt.Errorf("compare path: %s error: %s", path, err)
 }
--- equals_test.go
+++ equals_test.go
@@ -2,10 +2,69 @@ package fastjson
 
 import "testing"
 
+type EqualsTest struct {
+	a     string
+	b     string
+	error bool
+}
+
 func TestEquals(t *testing.T) {
 	
-	a := `{"a": 1, "b":{"b1":2, "b2": 3}, "d":[1,2,3]}`
-	b := `{"a": 1, "b":{"b1":2, "b2": 3}, "d": [1,3,2]}`
+	tests := []EqualsTest{
+		{
+			a:     `{"a": 1, "b":{"b1":2, "b2": 3}, "d":[1,2,3]}`,
+			b:     `{"a": 1, "b":{"b1":2, "b2": 3}, "d": [1,2,3]}`,
+			error: false,
+		},
+		{
+			a:     `{"a": 1, "b":{"b1":2, "b2": 3}, "d":[1,2,3]}`,
+			b:     `{"a": 3, "b":{"b1":2, "b2": 3}, "d": [1,2,3]}`,
+			error: true,
+		},
+		{
+			a:     `{"a": 1, "b":{"b1":2, "b2": 3}, "d":[1,2,3]}`,
+			b:     `{"a": 1, "b":{"b1":4, "b2": 3}, "d":[1,2,3]}`,
+			error: true,
+		},
+		{
+			a:     `{"a": 1, "b":{"b1": {"c": "cc"}, "b2": 3}, "d":[1,2,3]}`,
+			b:     `{"a": 1, "b":{"b1": {"c": "cx"}, "b2": 3}, "d":[1,2,3]}`,
+			error: true,
+		},
+		{
+			a:     `{"a": 1, "b":{"b2": 3}, "d":[1,2,3]}`,
+			b:     `{"a": 1, "b":{"b1":2, "b2": 3}, "d": [1,2,3]}`,
+			error: true,
+		},
+		{
+			a:     `{"a": 1, "b":{"b1": 1, "b2": 3}, "d":[2,3]}`,
+			b:     `{"a": 1, "b":{"b2": 3}, "d": [1,2,3]}`,
+			error: true,
+		},
+		{
+			a:     `{"a": 1, "b":{"b1":2, "b2": 3}, "d":[1,2,3]}`,
+			b:     `{"a": 1, "b":{"b1":2, "b2": 3}, "d": [1,3,2]}`,
+			error: true,
+		},
+		{
+			a:     `{"a": 1, "b":{"b1":2, "b2": 3}, "d":[{"a":1}]}`,
+			b:     `{"a": 1, "b":{"b1":2, "b2": 3}, "d": [{"a":2}]}`,
+			error: true,
+		},
+	}
 	
-	t.Log(Equals(a, b))
+	for i, v := range tests {
+		err := Equals(v.a, v.b)
+		if v.error {
+			if err == nil {
+				t.Fatalf("test %d expect error, got nil", i)
+			}
+			t.Logf("test %d error: %s", i, err)
+		} else {
+			if err != nil {
+				t.Fatalf("test %d unexpect error, got error: %s", i, err)
+			}
+		}
+	}
+	
 }
